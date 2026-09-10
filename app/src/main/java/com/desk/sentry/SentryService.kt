a7cb2a2748077e2a2f48fc5bd9f5f73257ab94f1
subodhcoc222-dev <subodhcoc222@gmail.com>
2026-09-10 11:54:41 +0530

    Update SentryService.kt

--- app/src/main/java/com/desk/sentry/SentryService.kt
+++ app/src/main/java/com/desk/sentry/SentryService.kt
@@ -18,6 +18,7 @@ import android.net.Uri
 import android.os.BatteryManager
 import android.os.Build
 import android.os.Handler
+import android.os.IBinder
 import android.os.Looper
 import android.os.PowerManager
 import androidx.core.app.NotificationCompat
